ae7a0c51c27d945639776585ab1593e4c5e6c641
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-07-28 11:13:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# YEETem Potions
+Throw the potions, don't just drop them!
+## Information on the CurseForge Page:
+[https://www.curseforge.com/minecraft/mc-mods/yeetem-potions](https://www.curseforge.com/minecraft/mc-mods/yeetem-potions)
---
+++ src/main/java/de/cas_ual_ty/yeet/YEETemPotions.java
@@ -0,0 +1,28 @@
+package de.cas_ual_ty.yeet;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(YEETemPotions.MODID)
+public class YEETemPotions
+{
+    public static final String MODID = "yeetem_potions";
+    
+    public YEETemPotions()
+    {
+        MinecraftForge.EVENT_BUS.addListener(this::yeet);
+    }
+    
+    private void yeet(EntityJoinLevelEvent event)
+    {
+        if(event.getEntity() instanceof ThrownPotion potion && potion.getOwner() instanceof Player)
+        {
+            Vec3 dir = potion.getOwner().getLookAngle();
+            potion.shoot(dir.x, dir.y, dir.z, 1.5F, 0F);
+        }
+    }
+}
